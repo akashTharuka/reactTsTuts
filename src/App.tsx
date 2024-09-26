b4d9e9096114a1f3be524b6bb31ad2013450d061
akashTharuka <akash_tharuka@yahoo.com>
2024-09-26 15:48:24 +0530

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import PersonList from "./components/PersonList";
 import Status from "./components/Status";
 import Heading from "./components/Heading";
 import Oscar from "./components/Oscar";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
 
@@ -40,6 +42,15 @@ function App() {
 			<Oscar>
 				<Heading>Placeholder text</Heading>
 			</Oscar>
+			<Button handleClick={(event, id) => {
+				console.log('Button Clicked', event, id);
+			}}/>
+			<Input 
+				value=""
+				handleChange={(event) => {
+					console.log(event.target.value);
+				}}
+			/>
 		</div>
     )
 }
